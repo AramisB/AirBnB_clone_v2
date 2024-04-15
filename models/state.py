365e4882bc52ee3b142716722d5defa30418a902
9-max-6 <mutukumaxwel@gmail.com>
2024-04-15 16:18:43 +0300

    New task

--- models/state.py
+++ models/state.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
-    
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade='all delete-orphan')
+
     def __init__(self, *args, **kwargs):
         """Initializes a new state"""
         super().__init__(*args, **kwargs)
+    
+    @property
+    def cities(self):
+        """getter for the cities attribute"""
+        return self.cities
