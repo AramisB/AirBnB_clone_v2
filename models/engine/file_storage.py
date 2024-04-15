ff30769532fb4adc11b532318372478bfd60f2b9
9-max-6 <mutukumaxwel@gmail.com>
2024-04-15 13:40:25 +0300

    New task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        else:
+            objects_per_class = {}
+            key = cls.__name__
+            for keys, obj in FileStorage.__objects.items():
+                if keys.startswith(key):
+                    objects_per_class[keys] = obj
+            return objects_per_class
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
