8d0927638cb5e6753b8411f55d6f3faa27872598
9-max-6 <mutukumaxwel@gmail.com>
2024-04-16 14:34:11 +0300

    New task

--- models/state.py
+++ models/state.py
@@ -9,7 +9,9 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state')
+    cities = relationship(
+        'City', backref='state', cascade="all, delete-orphan"
+        )
 
     def __init__(self, *args, **kwargs):
         """Initializes a new state"""
