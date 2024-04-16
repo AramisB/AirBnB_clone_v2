a4b49b9049237cd1a5c743ce96543f76401fc6c7
9-max-6 <mutukumaxwel@gmail.com>
2024-04-16 13:57:54 +0300

    Testing

--- models/state.py
+++ models/state.py
@@ -9,8 +9,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
+    cities = relationship('City', backref='state')
 
     def __init__(self, *args, **kwargs):
         """Initializes a new state"""
