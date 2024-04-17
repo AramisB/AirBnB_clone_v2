3c591bbe8bd6fb870c198e6edebffcc05e9f3cfa
9-max-6 <mutukumaxwel@gmail.com>
2024-04-17 20:53:22 +0300

    New task

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from os import getenv
-import models
+from models import storage
 from models.base_model import BaseModel
 from sqlalchemy import String, Column,  ForeignKey, Integer, Float, Table
 from models.base_model import Base
@@ -42,21 +42,14 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, default=0, nullable=True)
     longitude = Column(Float, default=0, nullable=True)
-    reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship(
-        "Amenity",
-        secondary="place_amenity",
-        backref="places",
-        viewonly=False
-    )
-    amenit_ids = []
+    amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
         def reviews(self):
             """Get a list of all linked Reviews."""
             review_list = []
-            for review in list(models.storage.all(Review).values()):
+            for review in list(storage.all(Review).values()):
                 if review.place_id == self.id:
                     review_list.append(review)
             return review_list
@@ -64,13 +57,18 @@ class Place(BaseModel, Base):
         @property
         def amenities(self):
             """Get/set linked Amenities."""
-            amenity_list = []
-            for amenity in list(models.storage.all(Amenity).values()):
-                if amenity.id in self.amenity_ids:
-                    amenity_list.append(amenity)
-            return amenity_list
+            return self.amenity_ids
 
         @amenities.setter
         def amenities(self, value):
             if type(value) == Amenity:
                 self.amenity_ids.append(value.id)
+    
+    if getenv("HBNB_TYPE_STORAGE", None) == "db":
+        reviews = relationship("Review", backref="place", cascade="delete")
+        amenities = relationship(
+            "Amenity",
+            secondary=association_table,
+            backref="places",
+            viewonly=False
+        )
