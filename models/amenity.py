7a2040a371623ea4c8c59d0951b3d08f1434cbad
9-max-6 <mutukumaxwel@gmail.com>
2024-04-17 12:03:06 +0300

    new task

--- models/amenity.py
+++ models/amenity.py
@@ -5,19 +5,31 @@ from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-association = Table('place_amenity',
-                    Base.metadata, 
-                    Column('place_id', ForeignKey('places.id'),
-                           primary_key=True),
-                           Column('amenity_id', ForeignKey('amenities.id'),
-                                  primary_key=True), 
-                                  viewonly=False
-                                  )
+association = Table(
+    'place_amenity',
+    Base.metadata, 
+    Column(
+        'place_id',
+        ForeignKey('places.id'),
+        primary_key=True,
+        nullable=False
+        ),
+    Column(
+        'amenity_id',
+        ForeignKey('amenities.id'),
+        primary_key=True,
+        nullable=False
+        ),
+    )
+
+
 class Amenity(BaseModel, Base):
     """The amenity class of the basemodel parent"""
     __tablename__ = 'amenities'
     name = Column(String(128))
-    place_amenities = relationship('Place',
-                                   secondary=association,
-                                   backref="amenities"
-                                   )
+    place_amenities = relationship(
+        'Place',
+        secondary=association,
+        backref="amenities",
+        viewonly=False
+        )
