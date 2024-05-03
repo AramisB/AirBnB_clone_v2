7f1a4248bbcefb471c2762f2316b2e33de79801c
9-max-6 <mutukumaxwel@gmail.com>
2024-05-03 20:22:29 +0300

    Fix: 1-pack_web_static.py:14:80: E501 line too long (90 > 79 characters)

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,7 +11,8 @@ def do_pack():
     """
     making an archive best on the web_static folder
     """
-    static_archive = 'web_static_' + datetime.now().strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    static_archive = 'web_static_' + datetime.now().strftime(
+        "%Y%m%d%H%M%S") + '.' + 'tgz'
     local('mkdir -p versions')
     create = local(f'tar -cvzf versions/{static_archive} web_static')
     if create is not None:
