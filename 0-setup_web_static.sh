565e0a08a5c71aab07c5cdd3165ca58aa7998f6b
AramisB <brendachem7@gmail.com>
2024-05-03 17:05:12 +0300

    a Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
+#a Bash script that sets up your web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow OpenSSH
