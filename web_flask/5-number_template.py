884afd44b75ab1c48b01ad1958c1ffd7e3e42518
AramisB <brendachem7@gmail.com>
2024-05-17 20:39:16 +0300

    A scripts that renders a HTML file

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+A script that starts Flask web application
+"""
+from flask import Flask, abort, render_template, redirect
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+@app.route('/')
+def hello():
+    """
+    A function that returns "Hello HBNB!"
+    """
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    A function that returns HBNB
+    """
+    return ('HBNB')
+
+
+@app.route('/c/<text>')
+def c(text):
+    """
+    A function that displays C followed by text input
+    """
+    text_with_spaces = text.replace('_', ' ')
+    return f"C {text_with_spaces}"
+
+
+@app.route('/python/', defaults={'text': 'is_cool'},)
+@app.route('/python/<text>')
+def python(text):
+    """
+    A function that displays Python followed by text input
+    default text input is "is fun"
+    """
+    text_with_spaces = text.replace('_', ' ')
+    return f"Python {text_with_spaces}"
+
+
+@app.route('/number/<n>')
+def number(n):
+    """
+    A function that displays a number only if it's an integer
+    """
+    try:
+        n = int(n)
+        return redirect(f"/number_template/{n}")
+
+    except ValueError:
+        abort(404)
+
+
+@app.route('/number_template/<n>')
+def number_template(n):
+    """
+    A function that displays a HTML page only n is an integer
+    """
+    try:
+        n = int(n)
+        return render_template('5-number.html', n=n)
+    except ValueError:
+        abort(404)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
