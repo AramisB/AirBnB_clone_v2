14ce8d79c588ef115a0d09a02d16f5e9dc9e96d6
AramisB <brendachem7@gmail.com>
2024-05-17 17:15:37 +0300

    route with a default text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A script that starts Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    A function that returns "Hello HBNB!"
+    """
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    A function that returns HBNB
+    """
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    A function that displays C followed by text input
+    """
+    text_with_spaces = text.replace('_', ' ')
+    return f"C {text_with_spaces}"
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """
+    A function that displays Python followed by text input
+    default text input is "is fun"
+    """
+    text_with_spaces = text.replace('_', ' ')
+    return f"Python {text_with_spaces}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
